a734f51ab790941a94732622d9686ea4412e1642
Jonathan Grunder <contact@jonathan-grunder.tech>
2020-10-30 17:55:49 +0100

    Convert all classic js catcher to jquery

--- assets/abstergo.js
+++ assets/abstergo.js
@@ -48,11 +48,11 @@ const settings = {
       charts.cpuTemp.chart.data.datasets[0].data[1] = parseFloat(data.payload.value - settings.max);
       charts.cpuTemp.chart.update();
     });
-    const cpuLoad = document.getElementById('cpu-current-load');
+    const cpuLoad = $('#cpu-current-load');
     MobroSDK.addChannelListener('general_processor_usage', data => {
       charts.cpuLoad.chart.data.datasets[0].data.push(parseInt(data.payload.value));
       charts.cpuLoad.chart.data.datasets[0].data.shift();
-      cpuLoad.innerHTML = parseInt(data.payload.value);
+      cpuLoad.html(parseInt(data.payload.value));
       charts.cpuLoad.chart.data.datasets[0].borderColor = getLineColor(parseInt(data.payload.value));
       charts.cpuLoad.chart.update();
     });
@@ -61,11 +61,11 @@ const settings = {
       charts.gpuTemp.chart.data.datasets[0].data[1] = parseFloat(data.payload.value - settings.max);
       charts.gpuTemp.chart.update();
     });
-    const gpuLoad = document.getElementById('gpu-current-load');
+    const gpuLoad = $('#gpu-current-load');
     MobroSDK.addChannelListener('general_graphics_usage', data => {
       charts.gpuLoad.chart.data.datasets[0].data.push(parseInt(data.payload.value));
       charts.gpuLoad.chart.data.datasets[0].data.shift();
-      gpuLoad.innerHTML = parseInt(data.payload.value);
+      gpuLoad.html(parseInt(data.payload.value));
       charts.gpuLoad.chart.data.datasets[0].borderColor = getLineColor(parseInt(data.payload.value));
       charts.gpuLoad.chart.update();
     });
@@ -74,12 +74,12 @@ const settings = {
       charts.ramUsage.chart.data.datasets[0].data[1] = parseFloat(100 - data.payload.value);
       charts.ramUsage.chart.update();
     });
-    const memoryData = document.getElementById('mobro-ram-data--used');
+    const memoryData = $('#mobro-ram-data--used');
     MobroSDK.addChannelListener('general_memory_used', data => {
-      memoryData.innerHTML = convert(data.payload.unit,'GB',data.payload.value).toFixed(2);
+      memoryData.html(convert(data.payload.unit,'GB',data.payload.value).toFixed(2));
     });
     MobroSDK.emit("monitor:hardware").then(data => {
-      document.getElementById("mobro-ram-data--total").innerHTML = convert(data.memory.capacityunit,'GB',data.memory.totalcapacity) + 'GB';
+      $("#mobro-ram-data--total").html(convert(data.memory.capacityunit,'GB',data.memory.totalcapacity) + 'GB');
     });
     MobroSDK.emit("monitor:sensor:data", "general_processor_temperature").then(data => {//could prefill line graphs later on...
     });
@@ -88,82 +88,82 @@ const settings = {
         return;
       }
 
-      document.getElementById("mobro-vram-data-total").innerHTML = convert(data.unit,'GB',data.value) + 'GB';
+      $("#mobro-vram-data-total").html(convert(data.unit,'GB',data.value) + 'GB');
     });
     // Convert Hexa color into css filter:
     // https://codepen.io/sosuke/pen/Pjoqqp
     MobroSDK.addChannelListener("theme_fan_speed_cpu", data => {
       if (data.payload && data.payload.sensortype) {
         var currentPercent = parseFloat(data.payload.value);
-        document.getElementById("cpu_fan_usage").innerHTML = currentPercent + "%";
+        $("#cpu_fan_usage").html(currentPercent + "%");
         if(currentPercent <= 50) {
-          document.getElementById("cpu_fan_img").style.filter = "invert(44%) sepia(92%) saturate(468%) hue-rotate(150deg) brightness(93%) contrast(96%)";
+          $("#cpu_fan_img").css('filter', "invert(44%) sepia(92%) saturate(468%) hue-rotate(150deg) brightness(93%) contrast(96%)");
         }
         else if(currentPercent <= 70) {
-          document.getElementById("cpu_fan_img").style.filter = "invert(50%) sepia(86%) saturate(1036%) hue-rotate(360deg) brightness(103%) contrast(106%)";
+          $("#cpu_fan_img").css('filter', "invert(50%) sepia(86%) saturate(1036%) hue-rotate(360deg) brightness(103%) contrast(106%)");
         }
         else {
-          document.getElementById("cpu_fan_img").style.filter = "invert(9%) sepia(78%) saturate(6113%) hue-rotate(18deg) brightness(87%) contrast(122%)";
+          $("#cpu_fan_img").css('filter', "invert(9%) sepia(78%) saturate(6113%) hue-rotate(18deg) brightness(87%) contrast(122%)");
         }
       } else {
-        document.getElementById("cpu_fan_usage").innerHTML = "0%";
-        document.getElementById("cpu_fan_img").style.filter = "invert(0%) sepia(91%) saturate(7500%) hue-rotate(317deg) brightness(97%) contrast(103%)";
+        $("#cpu_fan_usage").html("0%");
+        $("#cpu_fan_img").css('filter', "invert(0%) sepia(91%) saturate(7500%) hue-rotate(317deg) brightness(97%) contrast(103%)");
       }
     });
     MobroSDK.addChannelListener("theme_fan_speed_gpu", data => {
       if (data.payload) {
-        document.getElementById("gpu_rpm").innerHTML = parseFloat(data.payload.value) + " RPM";
+        $("#gpu_rpm").html(parseFloat(data.payload.value) + " RPM");
       } else {
-        document.getElementById("gpu_rpm").innerHTML = "0 RPM";
+        $("#gpu_rpm").html("0 RPM");
       }
     });
     MobroSDK.addChannelListener("theme_fan_usage_gpu", data => {
       if (data.payload) {
         var currentPercent = parseFloat(data.payload.value);
-        document.getElementById("gpu_fan_usage").innerHTML = currentPercent + "%";
+        $("#gpu_fan_usage").html(currentPercent + "%");
         if(currentPercent <= 50) {
-          document.getElementById("gpu_fan_img").style.filter = "invert(44%) sepia(92%) saturate(468%) hue-rotate(150deg) brightness(93%) contrast(96%)";
+          $("#gpu_fan_img").css('filter', "invert(44%) sepia(92%) saturate(468%) hue-rotate(150deg) brightness(93%) contrast(96%)");
         }
         else if(currentPercent <= 70) {
-          document.getElementById("gpu_fan_img").style.filter = "invert(50%) sepia(86%) saturate(1036%) hue-rotate(360deg) brightness(103%) contrast(106%)";
+          $("#gpu_fan_img").css('filter', "invert(50%) sepia(86%) saturate(1036%) hue-rotate(360deg) brightness(103%) contrast(106%)");
         }
         else {
-          document.getElementById("gpu_fan_img").style.filter = "invert(9%) sepia(78%) saturate(6113%) hue-rotate(18deg) brightness(87%) contrast(122%)";
+          $("#gpu_fan_img").css('filter', "invert(9%) sepia(78%) saturate(6113%) hue-rotate(18deg) brightness(87%) contrast(122%)");
         }
       } else {
-        document.getElementById("gpu_fan_usage").innerHTML = "0%";
-        document.getElementById("gpu_fan_img").style.filter = "invert(0%) sepia(91%) saturate(7500%) hue-rotate(317deg) brightness(97%) contrast(103%)";
+        $("#gpu_fan_usage").html("0%");
+        $("#gpu_fan_img").css('filter', "invert(0%) sepia(91%) saturate(7500%) hue-rotate(317deg) brightness(97%) contrast(103%)");
       }
     });
-    const vramData = document.getElementById("mobro-vram-data");
+    const vram = $("#vram-chart-doughnut");
+    const vramData = $("#mobro-vram-data");
     MobroSDK.addChannelListener("theme_vram", data => {
       if (data.payload) {
-        vramData.style.display = 'inline-block';
-        vramData.innerHTML = convert(data.payload.unit, 'GB', data.payload.value).toFixed(2);
+        vramData.css('display', 'inline-block');
+        vramData.html(convert(data.payload.unit, 'GB', data.payload.value).toFixed(2));
       } else {
-        vram.vramData.display = 'none';
+        vram.hide();
       }
     });
-    const vram = document.getElementById("vram-chart-doughnut");
     MobroSDK.addChannelListener("theme_vram_percentage", data => {
       if (data.payload) {
         if (!charts.vramUsage) {
-          charts.vramUsage = createDoughnuts(document.getElementById("vram-chart-doughnut"));
-          vram.style.display = 'block';
+          charts.vramUsage = createDoughnuts($("#vram-chart-doughnut"));
+          vram.show();
         }
 
         charts.vramUsage.chart.data.datasets[0].data[0] = parseFloat(data.payload.value);
         charts.vramUsage.chart.data.datasets[0].data[1] = parseFloat(100 - data.payload.value);
         charts.vramUsage.chart.update(); // vramData.innerHTML =
       } else {
-        vram.style.display = 'none';
+        vram.hide();
       }
     });
     MobroSDK.addChannelListener("theme_network_dl_usage", data => {
       charts.networkUsage.chart.data.datasets[0].data.push(parseInt(data.payload.value));
       charts.networkUsage.chart.data.datasets[0].data.shift();
       charts.networkUsage.chart.update();
-      document.getElementById("network-unit").innerHTML = data.payload.unit;
+      $("#network-unit").html(data.payload.unit);
     });
     MobroSDK.addChannelListener("theme_network_up_usage", data => {
       charts.networkUsage.chart.data.datasets[1].data.push(parseInt(data.payload.value));
@@ -222,12 +222,12 @@ function getLineColor(value) {
 
 function initCharts() {
   return {
-    "cpuLoad": createLine(document.getElementById("cpu-chart-line")),
-    "cpuTemp": createDoughnuts(document.getElementById("cpu-chart-doughnut")),
-    "gpuLoad": createLine(document.getElementById("gpu-chart-line")),
-    "gpuTemp": createDoughnuts(document.getElementById("gpu-chart-doughnut")),
-    "networkUsage": createLines(document.getElementById("network-chart-line")),
-    "ramUsage": createDoughnuts(document.getElementById("ram-chart-doughnut")),
+    "cpuLoad": createLine($("#cpu-chart-line")),
+    "cpuTemp": createDoughnuts($("#cpu-chart-doughnut")),
+    "gpuLoad": createLine($("#gpu-chart-line")),
+    "gpuTemp": createDoughnuts($("#gpu-chart-doughnut")),
+    "networkUsage": createLines($("#network-chart-line")),
+    "ramUsage": createDoughnuts($("#ram-chart-doughnut")),
     "vramUsage": null
   };
 }
@@ -417,7 +417,7 @@ function configureChartJS() {
           var min = Math.min(...chart.config.data.datasets[0].data, ...chart.config.data.datasets[1].data);
           var max = Math.max(...chart.config.data.datasets[0].data, ...chart.config.data.datasets[1].data);
           chart.config.options.scales.yAxes[0].ticks.stepSize = max;
-          if(document.getElementById("network-unit").innerHTML == 'KB/s') {
+          if($("#network-unit").html() == 'KB/s') {
             if(max < 100) {
               chart.config.options.scales.yAxes[0].ticks.max = 100;
               chart.config.options.scales.yAxes[0].ticks.min = 0;
